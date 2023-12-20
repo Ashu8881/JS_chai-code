ad5be50ff78188f6693184c10b62407fd4e9c1ec
Ashutosh <115519946+Ashu8881@users.noreply.github.com>
2023-12-20 05:02:05 +0000

    learn variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 90
+let name = "rahul"
+let emailid="008findme@gmail.com"
+accountCity="surat"
+let accounttitle
+// accountId=7
+console.log( accountId ); 
+/* never use var beacuse issue of scope and functional issue */
+console.log("rama");
+
+console.table([accountId,accountCity,emailid,name,accounttitle])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+// alert ("hello") alert cant be run in node need browser for it
+console.log(3+3)
+
+
+// data types 
+/* 
+number =>2 ^53
+string = " "
+boolan = true/falsh
+ null = a value
+undefined =  not asigned value
+symbol = unique 
+*/ 
+
+/* object */
+console.log(typeof undefined)
+console.log(typeof null)
