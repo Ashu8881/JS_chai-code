92cf39724d8c525e35307193fc9ed75f108edaa3
Ashutosh <115519946+Ashu8881@users.noreply.github.com>
2023-12-21 06:51:29 +0000

    conversation of diffrent data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+
+/* 
+conversation of data types  
+
+number,string & boolean
+
+*/
+
+
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+// let checknum = Number(score)
+//  console.log(typeof checknum)
+//  console.log(checknum)
+
+/* 
+* 11  => 11
+* 33sfsd =>NaN
+* string  => NaN
+* boolean => 1/0
+*/
+
+
+ let islogedin = 1 
+ let boolan = Boolean(islogedin)
+// console.log(boolan)
+
+
+/*
+
+ *1 => true
+ *  "" => false 
+ * anyvalue in string = true
+ * 
+ * 
+ * 
+ * 
+  */
+
+
+
+let string = 33
+let stringnum = String(string)
+console.log(typeof stringnum)
+/* 
+* 33 => string
+
+
+*/
