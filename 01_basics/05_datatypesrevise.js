bbb38830c1f12f0537b00967bfff435d5e0783d5
Ashutosh <115519946+Ashu8881@users.noreply.github.com>
2023-12-22 08:02:14 +0000

    revise datatypes

---
+++ 01_basics/05_datatypesrevise.js
@@ -0,0 +1,20 @@
+console.log(true)
+// premetive datatypes  
+/**string
+ * null
+ * number
+ * boolean
+ * undefined
+ * symbol
+ * BigInt
+ */
+
+//faadasd
+
+
+// Referance type
+/**
+ * array
+ * Objects
+ * Function
+ */
