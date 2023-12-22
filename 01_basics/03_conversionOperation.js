b5f3bb26516d441c7cd54e245a6a513d4274650c
Ashutosh <115519946+Ashu8881@users.noreply.github.com>
2023-12-22 06:45:31 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
 
 /* 
-conversation of data types  
+************************conversation of data types****************************  
 
-number,string & boolean
+                            *****number,string & boolean*******
 
 */
 
@@ -32,20 +32,37 @@ let score = true
 
  *1 => true
  *  "" => false 
- * anyvalue in string = true
- * 
- * 
- * 
- * 
+ * anyvalue in string = true 
   */
-
-
 
 let string = 33
 let stringnum = String(string)
-console.log(typeof stringnum)
+// console.log(typeof stringnum)
 /* 
 * 33 => string
-
-
 */
+
+// ******************************OPERATIONS****************************
+
+let value  = 3 
+let negvalue = -value
+// console.log(negvalue)
+// console.log(4+5)
+// console.log(7-3);
+// console.log(4*5);
+// console.log(6/3);
+// console.log(8%2)
+// console.log(2**3);
+ 
+
+let str1 = "hello"
+let str2 = "World"
+let newstr = str1+str2
+// console.log(newstr)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log('1'+2+2);
+// console.log(1+2+'2')
+// console.log(+true)
+// console.log(+"")
+// console.log("fgdg");
