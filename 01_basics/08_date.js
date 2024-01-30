a18c83c910c641b413daf551d84b797c1e1a4636
Ashu8881 <you@example.com>
2024-01-30 10:55:05 +0530

    date

--- 01_basics/08_date.js
+++ 01_basics/08_date.js
@@ -3,3 +3,8 @@ console.log(mydate);
 console.log(mydate.toString());
 console.log(mydate.toDateString());
 console.log(mydate.toLocaleDateString());
+console.log(typeof mydate);
+let mystamp = Date.now()
+console.log(mystamp);
+let myDate = new Date;
+console.log("fgsdgfgds"+myDate.getMonth());
