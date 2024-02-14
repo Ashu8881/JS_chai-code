9cd56b681cb901c3ba70bcb4c4a3db87523800e0
Ashu8881 <you@example.com>
2024-02-14 14:43:39 +0530

    function with obj and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,18 +1,18 @@
 function print(w) {
     console.log(w);
 }
-print('a')
-print('b')
-print('c')
-print('d')
+// print('a')
+// print('b')
+// print('c')
+// print('d')
 function addnumber(number1,number2){
     let result = number1 + number2
-    console.log(result)
+    // console.log(result)
     return result
     
 }
 const result =addnumber(5,7)
-console.log("result: ",result)
+// console.log("result: ",result)
 function loginuser( username = 'user'){
     if (username === undefined){
         console.log('please enter user name')
@@ -21,5 +21,35 @@ function loginuser( username = 'user'){
     console.log(`${username} logedin sucessfully `)
 return 
 }
-loginuser()
+// loginuser()
 // loginuser('ashutosh')
+
+//handle number with function and use of sprade operator
+
+
+
+function calculateprice(val1,val2,...num1){
+    return num1
+}
+console.log(calculateprice(3,300,343,34343,555))
+
+
+// handle object with function and object de structring in obj
+let user = {username:"ashutosh",
+id:'ORISD1354394'}
+function handleobj(anyobj){
+    console.log(`nameof user ${anyobj.username}` )
+    return
+}
+handleobj(user)
+
+
+//handle array with functions 
+
+
+let arr = [2,3,4,6,77,7889,34,30]
+
+ function arrayhandler(getarr){
+console.log(getarr[7],getarr[3],getarr[5])
+ }
+ arrayhandler(arr)
