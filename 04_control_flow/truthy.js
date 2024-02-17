fb42187eca84a26c5d1c7753f175b8474ecfd26e
Ashu8881 <you@example.com>
2024-02-17 15:07:07 +0530

    operators

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,19 @@
+const useremail ="oo@gmail.com"
+if (useremail){
+    console.log('got the mail id')
+}else{
+    console.log('unable to print')
+}
+
+//any string& array is true value, empty string & array  is false value
+
+
+// terneraay operator
+
+
+// condition ? true statment : false statment
+
+
+const price = 500
+
+price<300 ? console.log('need 200') : console.log('will get money') 
