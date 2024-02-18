ea4d53054ecf3b46c77068883f284fffcedbe3fa
Ashu8881 <you@example.com>
2024-02-18 13:40:46 +0530

    loops ,continue,break

---
+++ 05_iterations/one.js
@@ -0,0 +1,32 @@
+ 
+ 
+ for (let i = 0; i < 10; i++) {
+    const element = i;
+    // console.log(element)
+ }
+ 
+for (let i = 0; i < 10; i++) {
+    // console.log(`print${i}`)
+   for (let j = 0; j < 10; j++) {
+    
+    // console.log(`console${i}`)
+    // console.log(i  + '*'+ j + '='+ i *j )
+   }
+    
+}
+let array = ['flash','batman','superman']
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    // console.log(array.length)
+    // console.log(element)
+    // console.log(array.length)
+    
+}
+for (let index = 1; index <=17; index++) {
+
+    if(index ==5){
+        console.log(`value is 5`)
+        break }
+    console.log(`value of index = ${index}`)
+    
+}
