4a3bd4f89d2ea4855fdc9bced78a8663ba866629
Ashu8881 <you@example.com>
2024-02-18 14:34:54 +0530

    while loops do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    // console.log(element)
+}
+
+
+// let index = 10
+// while(index<20) {
+//     console.log(`printed ${index}`)
+// }
+array = ['flash','batman','superman']
+
+
+
+for(let i=0;i<20;i++){
+    // console.log(i)
+}
+
+
+let score = 500
+do {
+    console.log(`score is ${score}`)
+    score ++
+} while (score <=7);
